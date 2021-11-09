e35110616b3d51dc311199835b2fe6a31c8945d5
Thaison Nghiem <sonlamngan2015@gmail.com>
2021-11-09 16:50:07 -0700

    made working mode selection buttons

--- app/src/main/java/com/example/ninemenmorris/ModeSelectionScreen.java
+++ app/src/main/java/com/example/ninemenmorris/ModeSelectionScreen.java
@@ -5,21 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class ModeSelectionScreen extends AppCompatActivity {
-    public int gameMode;
-    public int gameDifficulty;
+    public int gameMode=2;
+    public int gameDifficulty=2;
+    Button pvpButton = (Button)findViewById(R.id.pvpButton);
+    Button pvcButton = (Button)findViewById(R.id.pvcButton);
+    Button easyButton = (Button)findViewById(R.id.easyButton);
+    Button mediumButton = (Button)findViewById(R.id.mediumButton);
+    Button hardButton = (Button)findViewById(R.id.hardButton);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode_selection_screen);
+        setDefaultGame();
     }
 
     public void updateGameModePvp(View myView){
         gameMode=1;
+        disableTheDifficulties();
     }
     public void updateGameModePvc(View myView){
         gameMode=2;
+        pvpButton.setEnabled(false);
+        enableTheDifficulties();
     }
     public void updateGameDifEasy(View myView){
         gameDifficulty=1;
@@ -39,6 +49,21 @@ public class ModeSelectionScreen extends AppCompatActivity {
         }
         startActivity(toGameScreen);
     }
-
-
+    private void disableTheDifficulties(){
+        easyButton.setEnabled(false);
+        mediumButton.setEnabled(false);
+        hardButton.setEnabled(false);
+    }
+    private void enableTheDifficulties(){
+        easyButton.setEnabled(true);
+        mediumButton.setEnabled(true);
+        hardButton.setEnabled(true);
+    }
+    private void setDefaultGame(){
+        pvcButton.setEnabled(true);
+        mediumButton.setEnabled(true);
+        pvpButton.setEnabled(false);
+        easyButton.setEnabled(false);
+        hardButton.setEnabled(false);
+    }
 }
