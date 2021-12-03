bb4177596d9ffd14e51a8f2dc96546fa596d5fba
James <jmmendoz@ualberta.ca>
2021-12-02 17:50:21 -0700

    Added Settings and Help screen Activities

--- app/src/main/java/com/example/ninemenmorris/StartScreen.java
+++ app/src/main/java/com/example/ninemenmorris/StartScreen.java
@@ -16,7 +16,6 @@ public class StartScreen extends AppCompatActivity {
 
         Button playGame = (Button) findViewById(R.id.playButton);
         Button creditsScreen = (Button) findViewById(R.id.creditsButton);
-
     }
     public void openModeSelection(View myView){
         Intent toModeSelect = new Intent(this, ModeSelectionScreen.class);
@@ -25,5 +24,13 @@ public class StartScreen extends AppCompatActivity {
     public void openCreditsScreen(View myView){
         Intent toCreditsScreen = new Intent(this, CreditsScreen.class);
         startActivity(toCreditsScreen);
+    }
+    public void openSettingsScreen(View myView){
+        Intent toSettingScreen = new Intent(this, SettingScreen.class);
+        startActivity(toSettingScreen);
+    }
+    public void toHelpScreen(View myView){
+        Intent goToHelpScreen = new Intent(this, HelpScreen.class);
+        startActivity(goToHelpScreen);
     }
 }
