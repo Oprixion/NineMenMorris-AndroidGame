a8b48aa079b6343e54343cab236130c681751f55
Thaison Nghiem <sonlamngan2015@gmail.com>
2021-12-03 22:23:44 -0700

    remove button after mill now works

--- app/src/main/java/com/example/ninemenmorris/PvpGameScreen.java
+++ app/src/main/java/com/example/ninemenmorris/PvpGameScreen.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.VibrationEffect;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,6 +17,7 @@ public class PvpGameScreen extends AppCompatActivity {
     private int player2PieceOnHand=9;
     private Button[] player1PieceArray = new Button[9];
     private Button[] player2PieceArray = new Button[9];
+    private Button[][] millOnBoardArray = new Button[9][9];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,24 +37,33 @@ public class PvpGameScreen extends AppCompatActivity {
         if ((player1PieceOnBoard+player1PieceOnHand > 2) || (player2PieceOnBoard+player2PieceOnHand > 2)){
             //When there are sill pieces on hand
             if (player1PieceOnHand > 0 && player2PieceOnHand > 0) {
-                 if (lastMove == "P1"){
+                if (lastMove=="P1"){
                     player2Turn(theMoves);
                     if(isMill()){
-                        removeOpponentPieceIfMill(theMoves);
+                        enableP1Moves();
+                        lastMove="p2RemoveTurn";
+                        //removeOpponentPieceIfMill(myView);
                     }
-                }
-                else if (lastMove == "P2"){
-                    player1Turn(theMoves);
+                 }
+                else if (lastMove=="P2"){
+                     player1Turn(theMoves);
                      if(isMill()){
-                         removeOpponentPieceIfMill(theMoves);
+                         enableP2Moves();
+                         lastMove="p1RemoveTurn";
+                         //removeOpponentPieceIfMill(myView);
                      }
-                }//else if
+                 }
+                else if((lastMove=="p1RemoveTurn")||(lastMove=="p2RemoveTurn")){
+                    removeOpponentPieceIfMill(myView);
+                    disableP2Moves();
+                    disableP1Moves();
+
+                }
             }//if
             //When there are no pieces left on hand
             if (player1PieceOnHand == 0 && player2PieceOnHand == 0){
 
             }//if
-
         }//if
     }//placeMove
 
@@ -76,9 +87,6 @@ public class PvpGameScreen extends AppCompatActivity {
         updateP1PieceCounterTV();
         lastMove="P1";
         textView.setText(lastMove);
-        if(isMill()){
-            enableP2Moves();
-        }
     }
 
     public void player2Turn(Button theMove){
@@ -91,9 +99,6 @@ public class PvpGameScreen extends AppCompatActivity {
         updateP2PieceCounterTV();
         lastMove="P2";
         textView.setText(lastMove);
-        if(isMill()){
-            enableP1Moves();
-        }
     }
 
     public void movePieceAdjacent(){
@@ -106,9 +111,10 @@ public class PvpGameScreen extends AppCompatActivity {
     }
 
 
-    public void removeOpponentPieceIfMill(Button toRemove){
+    public void removeOpponentPieceIfMill(View myView){
+        Button toRemove = (Button) myView;
         //human's turn
-        if (lastMove=="P1"){
+        if (lastMove=="p2RemoveTurn"){
             for (int i = 0; i< player1PieceArray.length; i++){
                 if (player1PieceArray[i]==toRemove){
                     player1PieceArray[i]=null;
@@ -116,9 +122,12 @@ public class PvpGameScreen extends AppCompatActivity {
             }
             toRemove.setText("");
             player2PieceOnBoard--;
+            lastMove="P2";
+
+
         }
 
-        if (lastMove=="P2"){
+        if (lastMove=="p1RemoveTurn"){
             for (int i = 0; i< player2PieceArray.length; i++){
                 if (player2PieceArray[i]==toRemove){
                     player2PieceArray[i]=null;
@@ -126,6 +135,8 @@ public class PvpGameScreen extends AppCompatActivity {
             }
             toRemove.setText("");
             player1PieceOnBoard--;
+            lastMove="P1";
+
         }
 
     }
@@ -166,8 +177,11 @@ public class PvpGameScreen extends AppCompatActivity {
     public boolean isMill() {
         //all moves on first square
         Button b00 = (Button) findViewById(R.id.pvpB00);
+        millOnBoardArray[0][0] = b00;
         Button b01 = (Button) findViewById(R.id.pvpB01);
+        millOnBoardArray[0][1] = b01;
         Button b02 = (Button) findViewById(R.id.pvpB02);
+        millOnBoardArray[0][2] = b02;
         Button b03 = (Button) findViewById(R.id.pvpB03);
         Button b04 = (Button) findViewById(R.id.pvpB04);
         Button b05 = (Button) findViewById(R.id.pvpB05);
@@ -194,58 +208,58 @@ public class PvpGameScreen extends AppCompatActivity {
 
         //first outer layer
         //check first layer above
-        if (b00.getText().equals(b01.getText()) && b01.getText().equals(b02.getText()))
+        if (b00.getText().equals(lastMove) && b01.getText().equals(lastMove) && b02.getText().equals(lastMove))
             return true;
         //check first layer left
-        if (b00.getText().equals(b03.getText()) && b03.getText().equals(b05.getText()))
+        if (b00.getText().equals(lastMove) && b03.getText().equals(lastMove) && b05.getText().equals(lastMove))
             return true;
         //check first layer right
-        if (b02.getText().equals(b04.getText()) && b04.getText().equals(b07.getText()))
+        if (b02.getText().equals(lastMove) && b04.getText().equals(lastMove) && b07.getText().equals(lastMove))
             return true;
         //check first layer below
-        if (b05.getText().equals(b06.getText()) && b06.getText().equals(b07.getText()))
+        if (b05.getText().equals(lastMove) && b06.getText().equals(lastMove) && b07.getText().equals(lastMove))
             return true;
 
         //second inner layer
         //check second layer above
-        if (b10.getText().equals(b11.getText()) && b11.getText().equals(b12.getText()))
+        if (b10.getText().equals(lastMove) && b11.getText().equals(lastMove) && b12.getText().equals(lastMove))
             return true;
         //check second layer left
-        if (b10.getText().equals(b13.getText()) && b13.getText().equals(b15.getText()))
+        if (b10.getText().equals(lastMove) && b13.getText().equals(lastMove) && b15.getText().equals(lastMove))
             return true;
         //check second layer right
-        if (b12.getText().equals(b14.getText()) && b14.getText().equals(b17.getText()))
+        if (b12.getText().equals(lastMove) && b14.getText().equals(lastMove) && b17.getText().equals(lastMove))
             return true;
         //check second layer below
-        if (b15.getText().equals(b17.getText()) && b17.getText().equals(b17.getText()))
+        if (b15.getText().equals(lastMove) && b17.getText().equals(lastMove) && b17.getText().equals(lastMove))
             return true;
 
         //third inner layer
         //check third layer above
-        if (b20.getText().equals(b21.getText()) && b21.getText().equals(b22.getText()))
+        if (b20.getText().equals(lastMove) && b21.getText().equals(lastMove) && b22.getText().equals(lastMove))
             return true;
         //check third layer left
-        if (b20.getText().equals(b23.getText()) && b23.getText().equals(b25.getText()))
+        if (b20.getText().equals(lastMove) && b23.getText().equals(lastMove) && b25.getText().equals(lastMove))
             return true;
         //check third layer right
-        if (b22.getText().equals(b24.getText()) && b24.getText().equals(b27.getText()))
+        if (b22.getText().equals(lastMove) && b24.getText().equals(lastMove) && b27.getText().equals(lastMove))
             return true;
         //check third layer below
-        if (b25.getText().equals(b26.getText()) && b26.getText().equals(b27.getText()))
+        if (b25.getText().equals(lastMove) && b26.getText().equals(lastMove) && b27.getText().equals(lastMove))
             return true;
 
         //middles
         //check middle above
-        if (b01.getText().equals(b11.getText()) && b11.getText().equals(b21.getText()))
+        if (b01.getText().equals(lastMove) && b11.getText().equals(lastMove) && b21.getText().equals(lastMove))
             return true;
         //check middle left
-        if (b03.getText().equals(b13.getText()) && b13.getText().equals(b23.getText()))
+        if (b03.getText().equals(lastMove) && b13.getText().equals(lastMove) && b23.getText().equals(lastMove))
             return true;
         //check middle right
-        if (b04.getText().equals(b14.getText()) && b14.getText().equals(b24.getText()))
+        if (b04.getText().equals(lastMove) && b14.getText().equals(lastMove) && b24.getText().equals(lastMove))
             return true;
         //check middle below
-        if (b06.getText().equals(b16.getText()) && b16.getText().equals(b26.getText()))
+        if (b06.getText().equals(lastMove) && b16.getText().equals(lastMove) && b26.getText().equals(lastMove))
             return true;
         return false;
     }
