baf56bf6327b5cc1ff72e00fb702be670594868a
James <jmmendoz@ualberta.ca>
2021-12-06 21:28:44 -0700

    Added mute button on StartScreen

--- app/src/main/java/com/example/ninemenmorris/SettingScreen.java
+++ app/src/main/java/com/example/ninemenmorris/SettingScreen.java
@@ -3,16 +3,45 @@ package com.example.ninemenmorris;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class SettingScreen extends AppCompatActivity {
-
+    MediaPlayer among;
+    private String onOff;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_screen);
+
+        Bundle onOffValue = getIntent().getExtras();
+        onOff = onOffValue.getString("myInfo");
+
+
+        Button muteButton = (Button) findViewById(R.id.muteButton);
+        among = MediaPlayer.create(SettingScreen.this,R.raw.among);
     }
+
+    public void toggleMute(View myView){
+        Button muteButton = (Button) myView;
+        muteButton.setText(onOff);
+
+        if(onOff.equals("Off")){
+            onOff = "On";
+            muteButton.setText(onOff);
+            among.start();
+        }
+        else{
+            onOff = "Off";
+            muteButton.setText(onOff);
+            among.pause();
+        }
+    }
+
+
+
     public void toHelpScreen(View myView){
         Intent goToHelpScreen = new Intent(this, HelpScreen.class);
         startActivity(goToHelpScreen);
--- app/src/main/java/com/example/ninemenmorris/StartScreen.java
+++ app/src/main/java/com/example/ninemenmorris/StartScreen.java
@@ -6,18 +6,44 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 
 public class StartScreen extends AppCompatActivity {
+    MediaPlayer among;
+    private String onOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
 
-        MediaPlayer alien= MediaPlayer.create(StartScreen.this,R.raw.alien);
-        alien.start();
+        onOff = "Off";
+
+        Button muteButton = (Button) findViewById(R.id.muteButton);
+        among = MediaPlayer.create(StartScreen.this,R.raw.among);
     }
+
+    public void toggleMute(View myView){
+        Button muteButton = (Button) myView;
+        muteButton.setText(onOff);
+
+        if(onOff.equals("Off")){
+            onOff = "On";
+            muteButton.setText(onOff);
+            among.start();
+        }
+        else{
+            onOff = "Off";
+            muteButton.setText(onOff);
+            among.pause();
+        }
+    }
+
     public void openModeSelection(View myView){
         Intent toModeSelect = new Intent(this, ModeSelectionScreen.class);
         startActivity(toModeSelect);
@@ -28,6 +54,7 @@ public class StartScreen extends AppCompatActivity {
     }
     public void openSettingsScreen(View myView){
         Intent toSettingScreen = new Intent(this, SettingScreen.class);
+        toSettingScreen.putExtra("myInfo", onOff);
         startActivity(toSettingScreen);
     }
     public void toHelpScreen(View myView){
