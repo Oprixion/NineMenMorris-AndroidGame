3a43bc1c09e3cd47121cf9a4bed6b60e8c405267
porcmetheus <92954491+porcmetheus@users.noreply.github.com>
2021-12-11 20:09:06 -0700

    Changed RestartScreen and PVP and added visual functions/assets

--- app/src/main/java/com/example/ninemenmorris/RestartScreen.java
+++ app/src/main/java/com/example/ninemenmorris/RestartScreen.java
@@ -3,6 +3,7 @@ package com.example.ninemenmorris;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class RestartScreen extends AppCompatActivity {
 
@@ -10,5 +11,30 @@ public class RestartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restart_screen);
+        //NEED A VARIABLE BUNDLED FROM PREVIOUS ACTIVITYS FOR THIS
+        boolean redWon = false;
+        if(redWon){
+            setRedWinnerHuman(true);
+        }
     }
+
+    /*
+     * Changes the winnerName to red and changes winnerPortrait red based on gamemode
+     * true - PVP gamemode "Changes portrait to human"
+     * false - PVE gamemode "Changes portrait to alien"
+     */
+    public void setRedWinnerHuman(boolean isPvp){
+        ImageView winnerName = (ImageView) findViewById(R.id.winnerName);
+        ImageView winnerPortrait = (ImageView) findViewById(R.id.winnerPortrait);
+
+        winnerName.setImageResource(R.drawable.indicator_red);
+
+        //Change portrait depending on mode
+        if(isPvp){
+            winnerPortrait.setImageResource(R.drawable.portrait_red_human_happy);
+        }
+        else{
+            winnerPortrait.setImageResource(R.drawable.portrait_red_alien_happy);
+        }
+    }//redIsWinner
 }
