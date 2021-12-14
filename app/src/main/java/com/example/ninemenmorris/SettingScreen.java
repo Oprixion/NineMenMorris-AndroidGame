2a9fde0f3e9d6e6c08982b42b9aa53f40763c510
James <jmmendoz@ualberta.ca>
2021-12-14 16:28:58 -0700

    pulling master to audio

--- app/src/main/java/com/example/ninemenmorris/SettingScreen.java
+++ app/src/main/java/com/example/ninemenmorris/SettingScreen.java
@@ -5,17 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupWindow;
 import android.widget.SeekBar;
 
 public class SettingScreen extends AppCompatActivity {
+    PopupWindow popUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_screen);
+
+        popUp = new PopupWindow(this);
 
         MusicService.musicInitialize = 2;
 
--- app/src/main/java/com/example/ninemenmorris/StartScreen.java
+++ app/src/main/java/com/example/ninemenmorris/StartScreen.java
@@ -4,13 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
-
-
-import android.widget.Button;
-import android.widget.ImageButton;
 
 public class StartScreen extends AppCompatActivity {
 
